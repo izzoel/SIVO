4bb3b93554c5b9b7c073a43e912cbf72d090ed09
izzoel <ramadhani.zulfahmi@gmail.com>
2025-07-05 12:38:46 +0800

    -fix tombol delete modal persediaan padat

--- resources/views/auth/scripts/modals.blade.php
+++ resources/views/auth/scripts/modals.blade.php
@@ -230,100 +230,97 @@
           });
       });
       $(document).on("click", ".D_B_padat", function() {
-                  let id = $(this).data("id").split('-').pop();
+          let id = $(this).data("id").split('-').pop();
 
-                  $(document).on('click', '.D_B_padat', function() {
-                      let id = $(this).data("id").split('-').pop();
+          $(".modalDelete").attr("id", "M_D_padat-" + id);
+          $("#M_D_padat-" + id).modal('show');
+          $("#D_route").attr('action', "/data/padat/destroy/" + id);
+      })
 
-                      $(".modalDelete").attr("id", "M_D_padat-" + id);
-                      $("#M_D_padat-" + id).modal('show');
-                      $("#D_route").attr('action', "/data/padat/destroy/" + id);
-                  })
+      $(document).on('click', '.U_B_laboratorium', function() {
+          let id = $(this).data("id").split('-').pop();
 
-                  $(document).on('click', '.U_B_laboratorium', function() {
-                      let id = $(this).data("id").split('-').pop();
+          $(".modalUpdate").attr("id", "M_U_laboratorium-" + id);
+          $("#M_U_laboratorium-" + id).modal('show');
+          $("#U_route").attr('action', "/setting/laboratorium/update/" + id);
 
-                      $(".modalUpdate").attr("id", "M_U_laboratorium-" + id);
-                      $("#M_U_laboratorium-" + id).modal('show');
-                      $("#U_route").attr('action', "/setting/laboratorium/update/" + id);
+          $.get("/setting/laboratorium/show/" + id, function(data) {
+              $("#U_nama").val(data.nama);
+              $("#U_lokasi").val(data.lokasi);
+          });
 
-                      $.get("/setting/laboratorium/show/" + id, function(data) {
-                          $("#U_nama").val(data.nama);
-                          $("#U_lokasi").val(data.lokasi);
-                      });
+          $("#U_stok").on('input', function() {
+              var value = $(this).val();
 
-                      $("#U_stok").on('input', function() {
-                          var value = $(this).val();
+              if (!$.isNumeric(value)) {
+                  $(this).val(value.replace(/[^0-9]/g, ''));
+              }
+          });
+      });
 
-                          if (!$.isNumeric(value)) {
-                              $(this).val(value.replace(/[^0-9]/g, ''));
-                          }
-                      });
-                  });
+      $(".D_B_laboratorium").click(function() {
+          let id = $(this).data("id").split('-').pop();
 
-                  $(".D_B_laboratorium").click(function() {
-                      let id = $(this).data("id").split('-').pop();
+          $(".modalDelete").attr("id", "M_D_laboratorium-" + id);
+          $("#M_D_laboratorium-" + id).modal('show');
+          $("#D_route").attr('action', "/setting/laboratorium/destroy/" + id);
+      })
 
-                      $(".modalDelete").attr("id", "M_D_laboratorium-" + id);
-                      $("#M_D_laboratorium-" + id).modal('show');
-                      $("#D_route").attr('action', "/setting/laboratorium/destroy/" + id);
-                  })
+      $(document).on('click', '.U_B_lokasi', function() {
+          let id = $(this).data("id").split('-').pop();
 
-                  $(document).on('click', '.U_B_lokasi', function() {
-                      let id = $(this).data("id").split('-').pop();
+          $(".modalUpdate").attr("id", "M_U_lokasi-" + id);
+          $("#M_U_lokasi-" + id).modal('show');
+          $("#U_route").attr('action', "/setting/lokasi/update/" + id);
 
-                      $(".modalUpdate").attr("id", "M_U_lokasi-" + id);
-                      $("#M_U_lokasi-" + id).modal('show');
-                      $("#U_route").attr('action', "/setting/lokasi/update/" + id);
+          $.get("/setting/lokasi/show/" + id, function(data) {
+              $("#U_nama").val(data.nama);
+              $("#U_jenis").val(data.jenis);
+          });
 
-                      $.get("/setting/lokasi/show/" + id, function(data) {
-                          $("#U_nama").val(data.nama);
-                          $("#U_jenis").val(data.jenis);
-                      });
+          $("#U_stok").on('input', function() {
+              var value = $(this).val();
 
-                      $("#U_stok").on('input', function() {
-                          var value = $(this).val();
+              if (!$.isNumeric(value)) {
+                  $(this).val(value.replace(/[^0-9]/g, ''));
+              }
+          });
+      });
 
-                          if (!$.isNumeric(value)) {
-                              $(this).val(value.replace(/[^0-9]/g, ''));
-                          }
-                      });
-                  });
+      $(".D_B_lokasi").click(function() {
+          let id = $(this).data("id").split('-').pop();
 
-                  $(".D_B_lokasi").click(function() {
-                      let id = $(this).data("id").split('-').pop();
+          $(".modalDelete").attr("id", "M_D_lokasi-" + id);
+          $("#M_D_lokasi-" + id).modal('show');
+          $("#D_route").attr('action', "/setting/lokasi/destroy/" + id);
+      })
 
-                      $(".modalDelete").attr("id", "M_D_lokasi-" + id);
-                      $("#M_D_lokasi-" + id).modal('show');
-                      $("#D_route").attr('action', "/setting/lokasi/destroy/" + id);
-                  })
+      $(document).on('click', '.U_B_satuan', function() {
+          let id = $(this).data("id").split('-').pop();
 
-                  $(document).on('click', '.U_B_satuan', function() {
-                      let id = $(this).data("id").split('-').pop();
+          $(".modalUpdate").attr("id", "M_U_satuan-" + id);
+          $("#M_U_satuan-" + id).modal('show');
+          $("#U_route").attr('action', "/setting/satuan/update/" + id);
 
-                      $(".modalUpdate").attr("id", "M_U_satuan-" + id);
-                      $("#M_U_satuan-" + id).modal('show');
-                      $("#U_route").attr('action', "/setting/satuan/update/" + id);
+          $.get("/setting/satuan/show/" + id, function(data) {
+              $("#U_nama").val(data.nama);
+              $("#U_jenis").val(data.jenis);
+          });
 
-                      $.get("/setting/satuan/show/" + id, function(data) {
-                          $("#U_nama").val(data.nama);
-                          $("#U_jenis").val(data.jenis);
-                      });
+          $("#U_stok").on('input', function() {
+              var value = $(this).val();
 
-                      $("#U_stok").on('input', function() {
-                          var value = $(this).val();
+              if (!$.isNumeric(value)) {
+                  $(this).val(value.replace(/[^0-9]/g, ''));
+              }
+          });
+      });
 
-                          if (!$.isNumeric(value)) {
-                              $(this).val(value.replace(/[^0-9]/g, ''));
-                          }
-                      });
-                  });
+      $(".D_B_satuan").click(function() {
+          let id = $(this).data("id").split('-').pop();
 
-                  $(".D_B_satuan").click(function() {
-                      let id = $(this).data("id").split('-').pop();
-
-                      $(".modalDelete").attr("id", "M_D_satuan-" + id);
-                      $("#M_D_satuan-" + id).modal('show');
-                      $("#D_route").attr('action', "/setting/satuan/destroy/" + id);
-                  })
+          $(".modalDelete").attr("id", "M_D_satuan-" + id);
+          $("#M_D_satuan-" + id).modal('show');
+          $("#D_route").attr('action', "/setting/satuan/destroy/" + id);
+      })
   </script>
